499b7b9dda28b786b996a6f3361137983c19738f
chinaza-max <mosesogbonna68@gmail.com>
2025-03-23 14:18:46 +0100

    test

--- src/db/index.js
+++ src/db/index.js
@@ -17,12 +17,12 @@ class DB {
       port: Number(serverConfig.DB_PORT),
       database: serverConfig.DB_NAME,
       logQueryParameters: true,
-      pool: {
+      /*  pool: {
         max: 4, // Maximum number of connections in the pool
         min: 0, // Minimum number of connections in the pool
         acquire: 30000, // The maximum time, in milliseconds, that pool will try to get a connection before throwing an error
         idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released
-      },
+      },*/
     };
 
     this.sequelize = new Sequelize(
