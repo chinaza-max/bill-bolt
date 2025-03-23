6ff781c5a4ada36dc577c51fb2c77fc7fd0e05f8
chinaza-max <mosesogbonna68@gmail.com>
2025-03-23 14:19:23 +0100

    connection issues

--- src/db/index.js
+++ src/db/index.js
@@ -18,7 +18,7 @@ class DB {
       database: serverConfig.DB_NAME,
       logQueryParameters: true,
       /*  pool: {
-        max: 4, // Maximum number of connections in the pool
+        max: 4, // Maximum number of connections in the poo
         min: 0, // Minimum number of connections in the pool
         acquire: 30000, // The maximum time, in milliseconds, that pool will try to get a connection before throwing an error
         idle: 10000, // The maximum time, in milliseconds, that a connection can be idle before being released
