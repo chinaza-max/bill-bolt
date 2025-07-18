46c70e30d130be59542ef9d7c6a733742b89a759
macBookpro <macbookpro@chinaza.local>
2025-07-18 04:49:51 -0700

    update

--- src/db/index.js
+++ src/db/index.js
@@ -38,7 +38,7 @@ class DB {
     if (serverConfig.NODE_ENV === 'development') {
      // await this.sequelize.sync({ alter: true });
      // await this.sequelize.sync({ force: true });
-     await this.updateExistingTransactionIds();
+   //  await this.updateExistingTransactionIds();
 
       try {
         await this.sequelize.query(`
