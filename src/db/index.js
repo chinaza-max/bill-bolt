6c1768815985fc8d505988de212da219efb12f00
macBookpro <macbookpro@chinaza.local>
2025-09-21 12:07:07 -0700

    update

--- src/db/index.js
+++ src/db/index.js
@@ -39,7 +39,7 @@ class DB {
     initModels(this.sequelize);
 
     if (serverConfig.NODE_ENV === 'development') {
-      await this.sequelize.sync({ alter: true });
+      //await this.sequelize.sync({ alter: true });
       // await this.sequelize.sync({ force: true });
       //  await this.updateExistingTransactionIds();
       //  await this.updateEmptyDisplayNames();
